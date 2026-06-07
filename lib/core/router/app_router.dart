1533a470e3bb3fb34af071cac88d5228b7115463
khilonjiya3 <pankajdoleyapdcl@gmail.com>
2026-06-08 00:24:54 +0530

    Update app_router.dart

--- lib/core/router/app_router.dart
+++ lib/core/router/app_router.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'dart:async';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 import '../../data/repositories/auth_repository.dart';
 
